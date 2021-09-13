d1d0187a1f2a063dbdc4ad6260ee1b5bf0efe8f6
rmnk-nz <rua.manukau20@gmail.com>
2021-09-13 11:24:58 +1000

    added end quiz function to calculate score

--- script.js
+++ script.js
@@ -1,62 +1,104 @@
 
 // decleared variables
 var questionsDiv = document.querySelector("#questionsDiv");
-var timerDisplay = document.querySelector("#timer");
-var choiceList = document.querySelector("#choiceList");
-var startBtn = document.querySelector("#startQuiz");
+var timerDisplay = document.querySelector("#timerDisplay");
+var optionList = document.querySelector("#optionList");
+var startBtn = document.querySelector("#startBtn");
 
 
-// object for each question with array to list the choices and answer
-var questionsOne = {
+// Objects to hold each question, [options] and answers
+var questionOne = {
     question: "How do you declare a JavaScript variable?",
-    choices: ["var myName", "var = myName", "variable myName"],
+    options: ["var myName", "var = myName", "variable myName"],
     answer: "var myName"
-};
-
+    };
 var questionTwo = {
     question:"String values must be enclosed within ____ when being assigned to variables.",
-    choices: ["curly brackets", "quotes", "commas", "parenthesis"],
+    options: ["curly brackets", "quotes", "parenthesis"],
     answer: "quotes"
-};
-
-var questionThree = {
+    };
+var quetionThree = {
     question: "What statement supplies the value of the function?",
-    choices: ["cancle", "result", "valueOf", "return"],
+    options: ["result", "valueOf", "return"],
     answer: "return"
-};
-
+    };
 var questionFour = {
     question: "Which of the following is the correct syntax of FOR?",
-    choices: ["for(test; intialize; increment)", "for(intialize; test; increment)", "for(test; increment; intialize)"],
+    options: ["for(test; intialize; increment)", "for(intialize; test; increment)", "for(test; increment; intialize)"],
     answer: "(intialize; test; increment)"
-}
+    };
 
 // variables for score
 var timeScore = 0;
-var questionScore = 0;
 
 // variables for timer
-var secondsLeft = 6;
-var timerPenalty = 10;
-var timerInterval; 
+var timerLimit = 6;
+var timerPenalty = 10; 
+var timerInterval = 0;
+
 
 //function to start timer and display to user
  function start() {
-    var timerInterval = setInterval(function() {
-        secondsLeft--;
-        timerDisplay.textContent = secondsLeft;
 
-        if(secondsLeft === 0) {
-            clearInterval(timerInterval);
-            timerDisplay.textContent = "TIMES UP"; 
-        }
+    if (timerInterval === 0) {
+        timerInterval = setInterval(function() {
+            timerLimit--;
+            timerDisplay.textContent = timerLimit;
 
-    }, 1000);
+            if (timerLimit <= 0) {
+                clearInterval(timerInterval);
+                endQuiz();
+                timerDisplay.textContent = "TIMES UP"; 
+            } 
+        }, 1000);
+    }
+    generateQuestionOne();   
 };
+
+//function to generate question
+function generateQuestionOne() {
+    //clear questionDiv to display question
+    questionsDiv.innerHTML = "";
+    var userQuestion = questionOne.question;
+    var userOptions = questionOne.options;
+    
+    questionsDiv.textContent = userQuestion;
+    
+    //display options for user to select
+    userOptions.forEach(function(newlist) {
+        var listItem = document.createElement("li");
+        listItem.textContent = newlist;
+        optionList.appendChild(listItem);
+        listItem.addEventListener("click", (compare));
+        console.log(listItem);
+    });
+    //compare userOption with answer
+    function compare(){
+
+    }
+    //apply penalty if incorrect else move to next question
+}
+
+function endQuiz() {
+    questionsDiv.innerHTML = "";
+    optionList.innerHTML = "";
+
+    //create heading 
+    var completeQuiz = document.createElement("h1");
+    completeQuiz.textContent = "Quiz Complete"
+    questionsDiv.appendChild(completeQuiz);
+
+    var finalScore = document.createElement("h2");
+    questionsDiv.appendChild(finalScore);
+
+    //calculate final score
+    if (timerLimit >= 0) {
+        var timeRemain = timerLimit;
+        clearInterval(timerInterval);
+        finalScore.textContent = "Final score: " + timeRemain;
+    }
+
+}
 
 // when clicked timer will start
 startBtn.addEventListener("click", start);
-
-
-
-
